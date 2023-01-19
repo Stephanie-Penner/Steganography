ea014b88475c77ad24fad0fda55cb5284e49507b
steph <steph@10.125.64.158>
2023-01-19 13:55:48 -0500

    Jan 19

--- Steganography/src/penner/stephanie/Encode.java
+++ Steganography/src/penner/stephanie/Encode.java
@@ -13,11 +13,12 @@ public class Encode {
 	 * @param image
 	 * @return - ints of binary of pixels with encoded message.
 	 */
-	private int[] getRGB(String binaryMessage, Image image) {
+	private static int[] getRGB(String binaryMessage, Image image) {
 		String[] rgbBinary = image.getRGBValues();
+		String origRGB;
 		
-		for (int i = 0; i < binaryMessage.length(); i++) {
-			String origRGB = rgbBinary[i];
+		for (int i = 0; i < binaryMessage.length()/2; i++) {
+			origRGB = rgbBinary[i];
 			rgbBinary[i] = origRGB.substring(0, origRGB.length()-2) + binaryMessage.substring(i*2, i*2 + 2);
 		}
 		
@@ -30,15 +31,15 @@ public class Encode {
 		return rgbInts;
 	}
 	
-	public void makeCodedImage (String filename, Image image, String message) {
+	public static void makeCodedImage (String filename, Image image, String messageBinary) {
 		BufferedImage imageEncoded = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
 		
-		imageEncoded.setRGB(0, 0, image.getWidth(), image.getHeight(), getRGB(message, image), 0, 0);
+		imageEncoded.setRGB(0, 0, image.getWidth(), image.getHeight(), getRGB(messageBinary, image), 0, 0);
 		
 		File outputImage = new File(filename);
 		
 		try {
-			ImageIO.write(imageEncoded, "png", outputImage);
+			ImageIO.write(imageEncoded, "jpg", outputImage);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
